74eb8c7b8c8621ff85893b29b6b0f869af5e2e35
CR3Y745 <creytas@gmail.com>
2021-11-04 22:21:08 +0100

    feat: create the signup ui

--- pages/signup.js
+++ pages/signup.js
@@ -1,11 +1,34 @@
 import Background from "../components/authentification/Background";
 import Header from "../components/authentification/Header";
+import { SignupStyle } from "../styles/authentification/signup";
 export default function Signup() {
-	return (
-		<>
-			<Background>
-				<Header />
-			</Background>
-		</>
-	);
+  return (
+    <>
+      <Background>
+        <Header />
+        <SignupStyle>
+          <form>
+            <h1>Inscription</h1>
+            <div className="user-id">
+              <input placeholder="Nom" />
+              <input placeholder="Prenom" />
+            </div>
+            <div>
+              <input placeholder="Pseudo" />
+            </div>
+            <div>
+              <input placeholder="Adresse email" />
+            </div>
+            <div>
+              <input placeholder="Mot de passe" />
+            </div>
+            <button type="submit">S'inscrire</button>
+          </form>
+          <div className="login">
+            Tu as deja un compte? <span>connexion</span>
+          </div>
+        </SignupStyle>
+      </Background>
+    </>
+  );
 }
---
+++ styles/authentification/signup.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const SignupStyle = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    color: #fff;
+  }
+  form {
+    position: absolute;
+    width: 398px;
+    height: 405px;
+    left: 521px;
+    top: 160px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    border: 0px solid #000;
+  }
+  form div {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 0px solid blue;
+  }
+  div input {
+    width: 100%;
+    background: #fff;
+  }
+  .user-id input {
+    width: 49%;
+  }
+  .login {
+    position: absolute;
+    width: 235px;
+    height: 18px;
+    left: 603px;
+    top: 570px;
+    color: #fff;
+    font-size: 15px;
+  }
+
+  button {
+    width: 100%;
+    border: 1px solid #fff;
+    box-sizing: border-box;
+    background-color: transparent;
+    color: #fff;
+    font-size: 18px;
+  }
+  input,
+  button {
+    padding: 1%;
+    border-radius: 10px;
+  }
+  span {
+    text-decoration: bold;
+  }
+`;

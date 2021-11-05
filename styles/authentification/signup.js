3c22392912159e3fa4aadfd788821f4e591d9204
CR3Y745 <creytas@gmail.com>
2021-11-05 13:32:46 +0100

    fix: define default signup style

--- styles/authentification/signup.js
+++ styles/authentification/signup.js
@@ -7,6 +7,10 @@ export const SignupStyle = styled.div`
     justify-content: space-between;
     align-items: center;
     border: 0px solid blue;
+    @media screen and (max-height: 450px) {
+		height: 40px;
+		margin-top: 20px;
+	}
     h1{
       color:#fff;
     }
@@ -30,11 +34,19 @@ export const SignupStyle = styled.div`
   input{
     border:none;
   }
-  button{
+  input::placeholder {
+		color: rgba(0, 0, 0, 0.5);
+	}
+  input:focus {
+		outline: none;
+		border: 3px solid rgba(0, 0, 0, 0.5);
+	}
+  button, button:focus{
     border:1px solid #fff;
     background:transparent;
     color:#fff;
     font-size:18px;
+    outline:none;
   }
   .user-id{
     width:100%;
@@ -53,6 +65,10 @@ export const SignupStyle = styled.div`
     color:#fff;
     text-decoration:none;
     font-weight:700;
+  }
+  a:hover{
+    cursor: pointer;
+			text-decoration: underline;
   }
  */}
 `;

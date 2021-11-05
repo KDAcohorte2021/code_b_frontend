a85bf226510ced51741a66fae756a475e3ae7533
CR3Y745 <creytas@gmail.com>
2021-11-05 10:12:40 +0100

    feat: update signup style

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,11 @@
 import Background from "../components/authentification/Background";
 import Header from "../components/authentification/Header";
+import {
+  Form,
+  InputStyle,
+  TextExplicatifStyle,
+  ButtonStyle,
+} from "../styles/authentification/login";
 import { SignupStyle } from "../styles/authentification/signup";
 import Link from "next/link";
 export default function Signup() {
@@ -7,28 +13,30 @@ export default function Signup() {
     <>
       <Background>
         <Header />
-        <SignupStyle>
-          <form>
-            <h1>Inscription</h1>
+
+        <Form>
+          <h1>Inscription</h1>
+          <SignupStyle>
             <div className="user-id">
-              <input name="" placeholder="Nom" />
-              <input name="" placeholder="Prenom" />
+              <InputStyle name="" placeholder="Nom" />
+              <InputStyle name="" placeholder="Prenom" />
             </div>
-            <div>
-              <input name="" placeholder="Pseudo" />
-            </div>
-            <div>
-              <input name="" placeholder="Adresse email" />
-            </div>
-            <div>
-              <input name="" placeholder="Mot de passe" />
-            </div>
-            <button type="submit">S'inscrire</button>
-          </form>
-          <div className="login">
-            Tu as deja un compte? <Link href="/login">connexion</Link>
-          </div>
-        </SignupStyle>
+          </SignupStyle>
+
+          <InputStyle name="" placeholder="Pseudo" />
+
+          <InputStyle name="" placeholder="Adresse email" />
+
+          <InputStyle name="" placeholder="Mot de passe" />
+
+          <ButtonStyle onClick={(e) => e.preventDefault()}>
+            S'inscrire
+          </ButtonStyle>
+        </Form>
+        <TextExplicatifStyle className="inscription">
+          Tu as deja un compte?
+          <Link href="/login"> connexion </Link>
+        </TextExplicatifStyle>
       </Background>
     </>
   );
--- styles/authentification/signup.js
+++ styles/authentification/signup.js
@@ -1,24 +1,6 @@
 import styled from "styled-components";
 
 export const SignupStyle = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  h1 {
-    color: #fff;
-  }
-  form {
-    width: 40%;
-    height: 335px;
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    border: 0px solid #000;
-  }
   form div {
     width: 100%;
     display: flex;
@@ -33,35 +15,5 @@ export const SignupStyle = styled.div`
   .user-id input {
     width: 48%;
   }
-  .login {
-    position: absolute;
-    width: 235px;
-    height: 18px;
-    left: 603px;
-    top: 512px;
-    color: #fff;
-    font-size: 15px;
-  }
-  .login a {
-    text-decoration: none;
-    font-weight: 700;
-    color: #fff;
-  }
-
-  button {
-    width: 100%;
-    border: 1px solid #fff;
-    box-sizing: border-box;
-    background-color: transparent;
-    color: #fff;
-    font-size: 18px;
-  }
-  input,
-  button {
-    padding: 1%;
-    border-radius: 10px;
-  }
-  span {
-    text-decoration: bold;
-  }
+ */}
 `;
